a538d696c9ec1fc0f7f30a1de09ebfb6230e4898
Juan Antonio Aldea Armenteros <cyberjuen@gmail.com>
2013-06-07 04:43:29 +0300

    resource list moved to another file

--- js/resourceManager.js
+++ js/resourceManager.js
@@ -115,132 +115,14 @@ ResourceManager.prototype.loadFinished = function () {
 ResourceManager.prototype.loadCompleted = function () {
     for (var i = 0; i < resources.length; i++) {
         if (resources[i].type == "script") {
-            break;
             var file = document.createElement('script');
             file.setAttribute("type", "text/javascript");
             file.innerHTML = this.scripts[resources[i].url];
             document.getElementsByTagName("head")[0].appendChild(file);
         }
     }
-
     main();
 }
 //resources = JSON.parse(JSON.stringify(resources));
 
-var resources = [
-
-    {
-        name: "src",
-        type: "script",
-        url: "js/box2D.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/sprites.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/core.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/levels.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/box2DShortcuts.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/physicsObject.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/bubble.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/block.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/rope.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/harpoonHandler.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/world.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/inputEngine.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/player.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/game.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/main.js"
-    },
-    {
-        name: "src",
-        type: "script",
-        url: "js/lives.js"
-    },
-    {
-        name: "bg1",
-        type: "img",
-        url: "./img/background1.png"
-    },
-    {
-        name: "sprites",
-        type: "img",
-        url: "./img/sprites.png"
-    },
-    {
-        name: "explosion",
-        type: "sound",
-        url: "./sound/explosion.wav"
-    },
-    {
-        name: "bounce",
-        type: "sound",
-        url: "./sound/bounce.wav"
-    },
-    {
-        name: "hit",
-        type: "sound",
-        url: "./sound/hit.wav"
-    },
-    {
-        name: "heart",
-        type: "img",
-        url: "./img/heart.png"
-    }
-];
-
 var RM = new ResourceManager();
-
-RM.loadResources(resources);
---
+++ js/resources.js
@@ -0,0 +1,119 @@
+/**
+ * Author: Juan Antonio Aldea Armenteros
+ * Date: 6/7/13
+ * Time: 4:38 AM
+ */
+
+var resources = [
+
+    {
+        name: "src",
+        type: "script",
+        url: "js/box2D.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/sprites.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/core.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/levels.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/box2DShortcuts.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/physicsObject.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/bubble.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/block.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/rope.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/harpoonHandler.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/world.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/inputEngine.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/player.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/game.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/main.js"
+    },
+    {
+        name: "src",
+        type: "script",
+        url: "js/lives.js"
+    },
+    {
+        name: "bg1",
+        type: "img",
+        url: "./img/background1.png"
+    },
+    {
+        name: "sprites",
+        type: "img",
+        url: "./img/sprites.png"
+    },
+    {
+        name: "explosion",
+        type: "sound",
+        url: "./sound/explosion.wav"
+    },
+    {
+        name: "bounce",
+        type: "sound",
+        url: "./sound/bounce.wav"
+    },
+    {
+        name: "hit",
+        type: "sound",
+        url: "./sound/hit.wav"
+    },
+    {
+        name: "heart",
+        type: "img",
+        url: "./img/heart.png"
+    }
+];
